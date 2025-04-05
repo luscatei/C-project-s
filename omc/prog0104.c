8e861631ef5cdd68e73150fd9185e4106f070a8f
Lucas Teixeira <43426203+luscatei@users.noreply.github.com>
2025-04-05 01:43:36 +0000

    nova ordenação

--- omc/prog0104.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    
-    printf("\nC\nis the greatest\nLanguage\n\n");
-
-    return 0;
-}
--- omc/prog0107.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    
-    printf("Aqui vai um apito\a\n");
-    printf("Aqui vai um apito\7\n");
-}
