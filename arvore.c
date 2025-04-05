d92da5e99a211fc1924bc061913b620bf9bafce3
Lucas Teixeira <43426203+luscatei@users.noreply.github.com>
2025-04-05 03:09:23 +0000

    criação de ´arvore.c´

--- arvore.c
+++ arvore.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
-    Aqui, um teste simples que mostra o poder da função "printf()."
+    Aqui, um teste simples que mostra o poder da função "printf(). "
 */
 
 int main(){
