42dac19bd7d9b9fa06899eac971ce38c0826d667
Lucas Teixeira <43426203+luscatei@users.noreply.github.com>
2025-04-06 03:30:54 +0000

    ´puts.c´imoplementation

--- puts.c
+++ puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(){
-
+    /*
+        Here I tested the functionalities of the puts() function.
+    */
     puts("Total\t=\t100%%\nIVA\t=\t17%%\nIRS\t=\t15%%\nLiq.\t=\t68%%\n");
 
     puts("Hello World.");
